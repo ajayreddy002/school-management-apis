bdc3e05e17c80735f8b64c8eac3c1471d633a329
Ajay Kumar Reddy  Sanivarapu <asanivarapu@knowledgeinfotech.com>
2019-08-26 13:14:07 +0530

    Added live db

--- middleware/db.middleware.js
+++ middleware/db.middleware.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
-
-const server = '127.0.0.1:27017'; // REPLACE WITH YOUR DB SERVER
+require("dotenv").config();
+const server = '127.0.0.0.1:27017'; // REPLACE WITH YOUR DB SERVER
 const database = 'school-management';      // REPLACE WITH YOUR DB NAME
 mongoose.set('useCreateIndex', true);
 class Database {
@@ -9,13 +9,32 @@ class Database {
     }
 
     _connect() {
-        mongoose.connect(`mongodb://${server}/${database}`, { useNewUrlParser: true })
-            .then(() => {
-                console.log('Database connection successful')
-            })
-            .catch(err => {
-                console.error('Database connection error')
-            })
+        if (process.env.NODE_ENV === 'production') {
+            const uri = "mongodb+srv://school:school@123@cluster0-1fivm.mongodb.net/school-management?retryWrites=true&w=majority";
+            mongoose.connect(uri, { useNewUrlParser: true })
+                .then(() => {
+                    console.log('Database connection successful');
+                })
+                .catch(err => {
+                    console.error('Database connection error')
+                })
+        } else {
+            // const uri = "mongodb+srv://school:school@123@cluster0-1fivm.mongodb.net/school-management?retryWrites=true&w=majority";
+            mongoose.connect(`mongodb://${server}/${database}`, { useNewUrlParser: true })
+                .then(() => {
+                    console.log('Database connection successful');
+                })
+                .catch(err => {
+                    console.error('Database connection error')
+                })
+        }
+        // const MongoClient = require('mongodb').MongoClient;
+        // const client = new MongoClient(uri, { useNewUrlParser: true })
+        // client.connect().then(() => {
+        //     console.log('Database connection successful')
+        // }).catch(err => {
+        //     console.error('Database connection error')
+        // })
     }
 }
 module.exports = new Database()
