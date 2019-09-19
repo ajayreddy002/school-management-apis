437a6444ed3ada233e42f0b94ad89708f0487a27
Ajay Kumar Reddy  Sanivarapu <asanivarapu@knowledgeinfotech.com>
2019-09-19 13:28:01 +0530

    Added cors

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var swaggerUi = require('swagger-ui-express'),
     swaggerDocument = require('./swaggerSchoolTemplate.json');
 var app = express();
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(cors);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
